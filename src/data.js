efc4e9ae59b8f962fadf64e5643b9e7085f8c2ac
emremataraci <mtrcemre@gmail.com>
2022-08-14 07:15:44 +0300

    First commit.

---
+++ src/data.js
@@ -0,0 +1,33 @@
+const questions = [
+    {
+      id: 1,
+      title: 'Web3 Nedir?',
+      info:
+        'lk kez Ethereum’un kurucularından Gavin Wood’un 2014’te kullandığı “Web 3” terimi, “token” denilen blok birimleri esas aldığı için günümüzdeki merkeziyetçilikten uzak olan yepyeni bir internet anlamına geliyor. ',
+    },
+    {
+      id: 2,
+      title: 'Validatör Nedir?',
+      info:
+        'Validator, blokzincir ağında kullanılan proof of stake mekanizmasının önemli bir parçasıdır. Proof of work sisteminde madencinin aldığı rolü üstlenirler ve cihazlar yerine blok ekleme ve doğrulama işlemlerini yaparlar. Blokzincir üzerindeki işlemler, yalnızca validator tarafından onaylandığında tamamlanmış olur. Yani, zincirin kilit halkasıdır.',
+    },
+    {
+      id: 3,
+      title: 'Zero Knowledge Proof Nedir?',
+      info:
+        'Sıfır bilgi ispatı, bilgisayar bilimlerinde veri güvenliği alanında kullanılan algoritmanın temel prensibi "bildiğiniz bilgiyi bir başkasına, bilgiyi ona vermeden ispat etmektir. Bilginizi ispat etme yönteminiz genel olarak ideal bir kriptografik fonksiyonunun varsayımlarıdır.',
+    },
+    {
+      id: 4,
+      title: "Zero Knowledge Proof'a örnek verebilir misiniz?",
+      info:
+        "İki üst ve renk körü arkadaş Arkadaşınızın kırmızı-yeşil renk körü (siz değilken) ve iki top hayaliniz edin: bir kırmızı ve biri yeşil, bunun yanında aynı . Arkadaşınıza tamamen aynı görünüyor ve aslında aslında olduklarından şüpheleniyor. Gercekte farklı renkte bilgisine sahip olmak istiyorsun, ama başka bir şey değil; özellikle hangisinin kırmızı, hangisinin yeşil top oldugunu açıklamakezsiniz.İşte ispat sistemi. İki topu arkadaşınıza veriyorsunuz ve o onları arkasına koyuyor. Sonra toplardan birini alıp arkasından çıkarır ve gösterir. Daha sonra tekrar arkasına yerleştirir ve ardından iki toptan birini ortaya çıkarmaktan seçer ve ikisinden bir olasılıkla seçer. Size 'Topu değiştirdim mi?' Diye soracak. Tüm bu prosedür daha sonra kadar tekrar edilir. Renklerine, tabii ki, değiştirilmediği kesin olarak söyleyebilirsin. Öte yandan, aynı renkteyse ve garanti edilemezlerse,% 50'den yüksek olasılıkla doğru tahmin etmenin hiçbir yolu yoktur. Her bir anahtarı / anahtarı olmayanları tanımlamada başarılı olma olasılığınız% 50, tüm anahtarlı / anahtar olmayanlarda başarılı başarılı olma sıfıra yaklaşır ('sağlamlık'). Siz ve arkadaşınız bu 'kanıtı' defalarca (söz 100 kez) tekrar ederseniz, arkadaşınız topların gerçekten farklı renkte ikna olmalıdır ('cumalık'). üçüncü kanıt sıfır bilgidir çünkü arkadaşınız hangi topun yeşil, hangisinin kırmızı asla öğrenmez; gerçekten de yetenek nasıl edeceği konusunda hiçbir bilgisi yok.",
+    },
+    {
+      id: 5,
+      title: 'Bu Proje Kim Tarafından Yapılmıştır?',
+      info:
+        'Bu proje Emre Mataracı tarafından egzersiz amaçlı yapılmıştır.',
+    },
+  ]
+  export default questions
